39613e69a6cb8137b4271ce042c63e1d63cc9423
Orange Robotics <orangerobotics@gmail.com>
2025-09-29 12:11:10 -0400

    almost finished IO for climber

--- src/main/java/frc/robot/subsystems/climber/ClimberIONeo.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIONeo.java
@@ -45,25 +45,16 @@ public class ClimberIONeo implements ClimberIO{
         inputs.suckerMotorAppliedCurrent = sucker.getOutputCurrent();
         inputs.lifterMotorAppliedCurrent = lifter.getOutputCurrent();
         
-    
-        Logger.recordOutput("placerGrabber/orangeWheelsNeo/faultFlags", sucker.getFaults().rawBits);
-        Logger.recordOutput("placerGrabber/orangeWheelsNeo/hasActiveFault", sucker.hasActiveFault());
-        Logger.recordOutput("placerGrabber/orangeWheelsNeo/warningFlags", sucker.getWarnings().rawBits);
-        Logger.recordOutput("placerGrabber/orangeWheelsNeo/hasActiveWarning", sucker.hasActiveWarning());
-        Logger.recordOutput("placerGrabber/orangeWheelsNeo/overcurrent", sucker.getWarnings().overcurrent);
-        Logger.recordOutput("placerGrabber/orangeWheelsNeo/busVoltage", sucker.getBusVoltage());
-        Logger.recordOutput("placerGrabber/orangeWheelsNeo/dutyCycle", sucker.getAppliedOutput());
+        Logger.recordOutput("climber/sucker/busVolts", sucker.getBusVoltage());
+        Logger.recordOutput("climber/sucker/amps", sucker.getOutputCurrent());
+        Logger.recordOutput("climber/sucker/dutyCycle", sucker.getAppliedOutput());
         Logger.recordOutput("placerGrabber/orangeWheelsNeo/supposedAppliedVolts", sucker.getAppliedOutput() * sucker.getBusVoltage());
-
-        Logger.recordOutput("placerGrabber/omniwheelsNeo/faultFlags", lifter.getFaults().rawBits);
-        Logger.recordOutput("placerGrabber/omniwheelsNeo/hasActiveFault", lifter.hasActiveFault());
-        Logger.recordOutput("placerGrabber/omniwheelsNeo/warningFlags", lifter.getWarnings().rawBits);
-        Logger.recordOutput("placerGrabber/omniwheelsNeo/hasActiveWarning", lifter.hasActiveWarning());
-        Logger.recordOutput("placerGrabber/omniwheelsNeo/overcurrent", lifter.getWarnings().overcurrent);
-        Logger.recordOutput("placerGrabber/omniwheelsNeo/busVoltage", lifter.getBusVoltage());
-        Logger.recordOutput("placerGrabber/omniwheelsNeo/dutyCycle", lifter.getAppliedOutput());
-        Logger.recordOutput("placerGrabber/omniwheelsNeo/supposedAppliedVolts", lifter.getAppliedOutput() * lifter.getBusVoltage());
-
+        
+        Logger.recordOutput("climber/lifter/busVolts", lifter.getBusVoltage());
+        Logger.recordOutput("climber/lifter/amps", lifter.getOutputCurrent());
+        Logger.recordOutput("climber/lifter/dutyCycle", lifter.getAppliedOutput());
+        Logger.recordOutput("climber/lifter/supposedAppliedVolts", lifter.getAppliedOutput() * lifter.getBusVoltage());
+       
     }
     
     @Override
